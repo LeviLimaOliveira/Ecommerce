8effb7b4de6917628d1cec978c002cce3f33daab
Levi Oliveira <lev.developersr1@gmail.com>
2023-12-25 19:33:01 -0300

    validações

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -206,6 +206,51 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+const form = document.getElementById('form')
+const campos = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+
+function setError(index) {
+  campos[index].style.border = '1px solid #e63636';
+  spans[index].style.display = 'block';
+}
+
+function removeError(index){
+  campos[index].style.border = '';
+  spans[index].style.display = 'none';
+}
+
+function numeroValidade() {
+  if(campos[0].value.length < 16){
+    setError(0)
+  } else {
+    removeError(0)
+  }
+}
+
+function nameValidate() {
+  if(campos[1].value.length < 3){
+    setError(1)
+  } else {
+    removeError(1)
+  }
+}
+
+function cvcValidate() {
+  if(campos[2].value.length < 3) {
+    setError(2)
+  } else {
+    removeError(2)
+  }
+}
+
+function cpfValidate() {
+  if(campos[3].value.length < 11) {
+    setError(3)
+  } else {
+    removeError(3)
+  }
+}
 
 
 
